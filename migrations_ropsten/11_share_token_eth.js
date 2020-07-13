7002e35ef7669c20a19f863f8960140f787ac912
zhutianchi <zhutianchi@mcarlo.com>
2020-07-13 19:06:04 +0800

    remove unused ropsten migration

--- migrations_ropsten/11_share_token_eth.js
+++
@@ -1,5 +0,0 @@
-const ShareToken = artifacts.require('token/ShareToken.sol');
-
-module.exports = async function (deployer, network, accounts) {
-    await deployer.deploy(ShareToken, "Mai2 ShareToken", "STK", 18, { gas: 1500000 });
-};
--- migrations_ropsten/13_proxy_eth.js
+++
@@ -1,8 +0,0 @@
-const Perpetual = artifacts.require('perpetual/Perpetual.sol');
-const Proxy = artifacts.require('proxy/Proxy.sol');
-
-module.exports = async function (deployer, network, accounts) {
-    const perpetual = await Perpetual.deployed();
-
-    await deployer.deploy(Proxy, perpetual.address, { gas: 2600000 });
-};
